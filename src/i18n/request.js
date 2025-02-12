326f46844387f63b54eafec62e94723214b3828f
bashar8606 <ahammed.bashar9@gmail.com>
2025-02-12 19:03:31 +0400

    latest

--- src/i18n/request.js
+++ src/i18n/request.js
@@ -5,9 +5,13 @@ import { routing } from './routing';
 export default getRequestConfig(async ({ requestLocale }) => {
   // Validate that the incoming `locale` parameter is valid
   let locale = await requestLocale;
-  if (!routing.locales.includes(locale)) notFound();
+  // if (!routing.locales.includes(locale)) notFound();
+   if (!locale || !routing.locales.includes(locale)) {
+       locale = routing.defaultLocale;
+    }
   const currentLocale = locale?.split(",")[0]?.split("-")[0]
   return {
+    locale,
     messages: (
       await (currentLocale === 'en'
         ? // When using Turbopack, this will enable HMR for `en`
@@ -17,6 +21,28 @@ export default getRequestConfig(async ({ requestLocale }) => {
   };
 });
 
+
+// + import {routing} from './i18n/routing';
+ 
+// export default getRequestConfig(async ({
+// -  locale
+// +  requestLocale
+// }) => {
+// +  // This typically corresponds to the `[locale]` segment
+// +  let locale = await requestLocale;
+ 
+// -  // Validate that the incoming `locale` parameter is valid
+// -  if (!routing.locales.includes(locale as any)) notFound();
+// +  // Ensure that the incoming locale is valid
+// +  if (!locale || !routing.locales.includes(locale as any)) {
+// +    locale = routing.defaultLocale;
+// +  }
+ 
+//   return {
+// +    locale,
+//     // ...
+//   };
+// });
 
 // import {getRequestConfig} from 'next-intl/server';
 // import {routing} from '@/i18n/routing';

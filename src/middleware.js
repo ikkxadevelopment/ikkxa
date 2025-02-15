0ac1b9744b7dafa3b3e3316d5b86afd15611b574
bashar8606 <ahammed.bashar9@gmail.com>
2025-02-15 19:10:31 +0400

    latest

--- src/middleware.js
+++ src/middleware.js
@@ -76,6 +76,7 @@
 
 // export { auth as middleware } from "@/auth"
 
+
 import { getToken } from "next-auth/jwt";
 import createMiddleware from "next-intl/middleware";
 import { routing } from "@/i18n/routing";
@@ -88,20 +89,13 @@ const middleware = async (req, ev) => {
   const isProtectedRoute = protectedPaths.some((path) =>
     req.nextUrl.pathname.includes(path)
   );
-  
-  const cookies = req.cookies;
-  const nextLocale = cookies.get('NEXT_LOCALE')?.value;
-  let selectedCountryCode = nextLocale.match(/-(\w+)$/)?.[1]
 
-  // If no country is selected manually, use the geo-detected country
-  if (!selectedCountryCode) {
-    // Check geo location
-    const detectedCountry = req.geo?.country || 'SA'; // Default to Saudi if geo is unavailable
-    selectedCountryCode = detectedCountry === 'AE' ? 'AE' : 'SA';
-  }
+  // Check geo location
+  const countryCode = req.geo?.country || 'SA'; // Default to Saudi Arabia if geo is unavailable
+
 
   const currentLocale = req.nextUrl.pathname.startsWith('/ar') ? 'ar' : 'en';
-  const newPathname = `/${currentLocale}-${selectedCountryCode}`;
+  const newPathname = `/${currentLocale}-${countryCode}`;
 
   if (!req.nextUrl.pathname.startsWith(newPathname)) {
     return NextResponse.redirect(new URL(newPathname, req.url));
@@ -116,9 +110,10 @@ export const config = {
   matcher: [
     '/',
     '/(en-SA|en-AR|ar-SA|ar-AE)/:path*',
-    '/((?!api|_next|_vercel|.\\..).*)'
+    '/((?!api|_next|_vercel|.*\\..*).*)'
   ]
 };
+
 
 
 

f58ee9686c0de6010966ec83ab4587a1adeb3d48
bashar8606 <ahammed.bashar9@gmail.com>
2025-02-28 19:10:16 +0400

    latest

--- src/app/[lang]/products/page.js
+++ src/app/[lang]/products/page.js
@@ -1,9 +1,27 @@
 
 import Products from "@/widgets/Products";
 
+export async function generateMetadata() {
+  return {
+    title: "All Products | Online Shopping in GCC",
+    description:
+      "Search for Jalabiyas, abayas, lehengas, baby products and more on ikkxa",
+    // alternates: {
+    //   canonical: canonicalUrl,
+    // },
+    robots: {
+      index: false,
+      follow: true,
+    },
+  };
+}
+
+
+
+
 export default async function ProductsPage() {
     return (
-        <main className="min-h-screen pt-32">
+        <main className="min-h-screen  pt-[60px] lg:pt-20">
             <Products />
         </main>
     );

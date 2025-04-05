383a2b956ce8d930ba727d400057cc16f640b9d0
bashar8606 <ahammed.bashar9@gmail.com>
2025-04-05 07:20:33 +0530

    lae

--- src/components/EmiComponent/index.js
+++ src/components/EmiComponent/index.js
@@ -1,8 +1,10 @@
-import { useTranslations } from "next-intl";
+import { useLocale, useTranslations } from "next-intl";
 import Image from "../Image/image";
 
 export default function EmiComponent({type, price}) {
   const t = useTranslations("Index");
+  const lang = useLocale();
+  const [locale, country] = lang.split('-');
   return (
     <>
     {type==="tabby"?
@@ -10,12 +12,18 @@ export default function EmiComponent({type, price}) {
     <div className="aspect-[46/17] w-11 absolute top-0 left-3 -translate-y-1/2">
       <Image src={"/images/tabby_logo.png"} fill className="object-contain" alt="tabby logo" />
     </div>
-    {t('tabyEmiTitle')} {price/4}.{t('LearnMore')}
+    {country==="SA"?<>
+    {t('tabyEmiTitle')} {price/4}.{t('LearnMore')}</>:
+
+    <>{t('SplitYourPayment')} </>}
   </div>:<div className="px-3 py-4 rounded-sm border border-red-400  text-black text-xs mb-5 relative">
     <div className="aspect-[46/17] w-11 absolute top-0 left-3 -translate-y-1/2">
       <Image src={"/images/tamara_logo.png"} fill className="object-contain" alt="tamara logo" />
     </div>
-    {t('tabyEmiTitle')} {price/4}.{t('LearnMore')}
+    {country==="SA"?<>
+    {t('tabyEmiTitle')} {price/4}.{t('LearnMore')}</>:
+
+    <>{t('SplitYourPayment')} </>}
   </div>}
     </>
    

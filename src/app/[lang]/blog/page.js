ddf02cea905cf48ddfbf8c512be120ca648b3b42
bashar8606 <ahammed.bashar9@gmail.com>
2025-05-25 04:00:34 -0700

    deleted unwanted file

--- src/app/[lang]/blog/page.js
+++
@@ -1,9 +0,0 @@
-import BlogWidget from "@/widgets/BlogWidget";
-
-export default function BlogPage() {
-    return (
-        <main  className="min-h-screen pt-20">
-            <BlogWidget />
-        </main>
-    )
-}
--- src/app/[lang]/blogs/page.js
+++ src/app/[lang]/blogs/page.js
@@ -1,24 +1,6 @@
-// import { getSingleMedia } from "@/lib/getPages";
 import BlogListing from "@/widgets/BlogListing";
-import DetailWidget from "@/widgets/DetailWidget";
-import { notFound } from "next/navigation";
-
-
-// export async function generateMetadata({ params: { slug, lang } }) {
-//     const data = await getSingleMedia(slug, lang);
-//     const seo = data?.data?.seo
-//     // console.log("meta madia dataaaaaaaaaaaaaaa",data);
-//     return {
-//         title: seo?.metaTitle,
-//         description: seo?.metaDescription,
-//         // keywords: seo?.meta_keywords,
-//     };
-// }
-
 
 export default async function LeaderSinglePage({ params: { slug, lang } }) {
-    // const data = await getSingleMedia(slug, lang)
-    // if (!data) return notFound();
     
     return (
         <main className="min-h-screen pt-20">

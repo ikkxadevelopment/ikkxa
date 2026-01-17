250de29a8665701cf67e2844314de2aa01949425
bashar8606 <ahammed.bashar9@gmail.com>
2026-01-17 13:53:45 +0530

    sdf

--- src/app/[lang]/blogs/[slug]/page.js
+++ src/app/[lang]/blogs/[slug]/page.js
@@ -7,6 +7,7 @@ export default async function LeaderSinglePage({ params: { slug, lang } }) {
     return (
         <main>
             <DetailWidget />
+            Blog page
         </main>
     )
 }
